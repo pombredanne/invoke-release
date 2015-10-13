62875b721a7040225e969762c30e4071c4c4c275
Nick Williams <nwilliams@eventbrite.com>
2015-10-13 11:44:05 -0500

    Added necessary files for setup and release.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+sys.path.insert(0, 'python')
+
+from invoke_release import __version__
+
+packages = [x for x in find_packages('python') if '.tests' not in x]
+
+# No dependencies to keep the library lightweight
+install_requires = [
+]
+
+test_requirements = [
+]
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (__version__, __version__))
+    os.system('git push --tags')
+    sys.exit()
+
+
+setup(
+    name='invoke_release',
+    version=__version__,
+    description='Reusable command-line release tasks for Eventbrite libraries and services.',
+    packages=packages,
+    package_dir={
+        '': 'python',
+    },
+    install_requires=install_requires,
+    author='Nick Williams',
+    author_email='nwilliams@eventbrite.com',
+    url='https://github.com/eventbrite/invoke-release',
+    # Invalid classifier prevents accidental upload to PyPI
+    classifiers=['Private :: Do Not Upload'],
+    test_suite='tests',
+    tests_require=test_requirements
+)
---
+++ tasks.py
@@ -0,0 +1,7 @@
+import sys
+
+# Let's eat our own dog food and make sure this works.
+sys.path.insert(0, 'python')
+from invoke_release.tasks import *
+
+configure_release_parameters(module_name='invoke_release', display_name='Invoke Release Library')
