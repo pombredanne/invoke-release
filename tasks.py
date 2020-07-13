068d594960e4561a5421ce44efdddb7e6f0bf371
Fernando Mez <fmezzabotta@eventbrite.com>
2020-07-13 17:28:24 -0300

    Update codeowners (#44)
    * Update codeowners
    
    * Fix flake8 issues

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ import sys
 
 # Let's eat our own dog food and make sure the local version works for releasing itself
 sys.path.insert(0, os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'python')))
-from invoke_release.tasks import *  # noqa: F403
+from invoke_release.tasks import *  # noqa: F403,E402
 from invoke_release.plugins import (  # noqa: E402
     PatternReplaceVersionInFilesPlugin
 )
